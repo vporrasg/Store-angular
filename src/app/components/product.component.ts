c902b1c55d4d5804d5fe99c977f5d12405d860f2
vporrasg <53625582+vporrasg@users.noreply.github.com>
2020-12-22 19:44:01 -0600

    1

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,34 @@
+import { 
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges 
+} from '@angular/core';
+
+import { Product } from '../product.model'
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html'
+})
+export class ProductComponent implements OnChanges{
+
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('constructor');
+    }
+    addCart() {
+        console.log('añadir al carrito');
+        this.productClicked.emit(this.product.id);
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('ngOnChanges');
+        console.log(changes);
+    }
+
+}
