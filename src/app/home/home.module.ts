6360e659d1c49399da4766c2eb4f5f19385fec6b
vporrasg <53625582+vporrasg@users.noreply.github.com>
2021-01-14 00:26:49 -0600

    5

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from './../shared/shared.module'
+
 
 @NgModule({
     declarations: [
@@ -13,7 +15,8 @@ import { HomeRoutingModule } from './home-routing.module';
     ],
     imports: [
         CommonModule,
-        HomeRoutingModule        
+        HomeRoutingModule,
+        SharedModule        
     ]
 })
 
